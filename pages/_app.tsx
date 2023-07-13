d6cf6790039b422a2696d08a72b578384e8ae3d3
Phanuphong Machai <phanuphong.forwork@gmail.com>
2023-07-14 02:19:22 +0700

    feat: load data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
+// `@chakra-ui/theme` is a part of the base install with `@chakra-ui/react`
+import chakraTheme from "@chakra-ui/theme";
+
+const components = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    ...components,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraBaseProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraBaseProvider>
+  );
 }
